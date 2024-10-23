f1544b562f9185e9cb45aacfe1391b16ac450832
Your Name <you@example.com>
2024-10-23 13:18:04 +0300

    Added functions

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -1,15 +1,38 @@
 <?php
 
-namespace App\Http\Controller;
+namespace App\Http\Controllers;
 
-use Illuminate\Http\Reuqest;
+use Illuminate\Http\Request;
 use App\Models\Note;
 
 class NoteController extends Controller
 {
+//1
     public function index()
     {
-        $posts = Note::all();
+        $notes = Note::all();
         return view ('notes.index', ['allNotes' => $notes]);
+    }
+
+
+//2
+    public function create()
+    {
+        return view ('notes.create');
+    }
+
+
+//3
+    public function store(Request $request)
+    {
+        // \log::debug($request);
+        $data = [
+            'title' => $request->title,
+            'content' => $request->content
+        ];
+
+        Note::create($data);
+
+        return redirect('/notes');
     }
 }
