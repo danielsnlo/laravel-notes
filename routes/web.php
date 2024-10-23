d3a79add0db25fb57ce5f95035b925ed5d22aea7
Your Name <you@example.com>
2024-10-23 13:19:55 +0300

    Added store route

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,5 @@ Route::get('/', function () {
 });
 
 Route::get('/notes', [NoteController::class, 'index']);
+Route::get('/notes/create', [NoteController::class, 'create']);
+Route::post('/notes', [NoteController::class, 'store']);
